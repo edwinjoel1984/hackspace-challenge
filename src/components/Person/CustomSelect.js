34658cf4973be69ece3187cb108bdcbdc1070257
Edwin Ulloa <edwinjoel1984@gmail.com>
2018-09-07 11:57:05 -0500

    add sttyle to select

--- src/components/Person/CustomSelect.js
+++ src/components/Person/CustomSelect.js
@@ -1,12 +1,14 @@
 import React from 'react';
 const CustomSelect = (props) => {
     return (
-        <select id="test" value={props.selectedApp.app_id} onChange={(e)=>props.changeOption(props.selectedApp.user_id,e.target.value)} >
-            <option value=''>Select App</option>
-            {props.options.map((option)=>(
-                <option key={option.id} value={option.id}>{option.name}</option>
-            ))}
-        </select>
+        <div className="form-group">
+            <select id="test" className="form-control" value={props.selectedApp.app_id} onChange={(e)=>props.changeOption(props.selectedApp.user_id,e.target.value)} >
+                <option value=''>Select App</option>
+                {props.options.map((option)=>(
+                    <option key={option.id} value={option.id}>{option.name}</option>
+                ))}
+            </select>
+        </div>
     )
 };
 
